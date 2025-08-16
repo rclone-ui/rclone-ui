789baee6d6fc08af6046d4b5ff3b3996b4b07615
FTCHD <144691102+FTCHD@users.noreply.github.com>
2025-08-17 00:53:16 +0200

    enable ftp support
    Signed-off-by: FTCHD <144691102+FTCHD@users.noreply.github.com>

--- lib/rclone/api.ts
+++ lib/rclone/api.ts
@@ -3,7 +3,7 @@ import { platform } from '@tauri-apps/plugin-os'
 import { useStore } from '../store'
 
 /* UTILS */
-const SUPPORRTED_BACKENDS = ['sftp', 's3', 'b2', 'drive', 'dropbox']
+const SUPPORRTED_BACKENDS = ['sftp', 's3', 'b2', 'drive', 'dropbox', 'ftp']
 
 function getAuthHeader() {
     return
